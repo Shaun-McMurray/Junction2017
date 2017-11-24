0e30ed6febae976a023518db11f413f90e43a4a9
jeberlen <guseberjo@student.gu.se>
2017-11-24 22:21:07 +0100

    Finished parser and Api call

---
+++ EnergyReality/Assets/scripts/Parser.cs
@@ -0,0 +1,22 @@
+﻿using SimpleJSON;
+
+
+public class Parser
+{
+    public FortumJson[] Json { get; set; }
+}
+
+public class FortumJson
+{
+    public int location { get; set; }
+    public string city { get; set; }
+    public int zipcode { get; set; }
+    public int amountofinhabitants { get; set; }
+    public int buildingyear { get; set; }
+    public int cubicmeters { get; set; }
+    public int electricityheatingsurface { get; set; }
+    public int squaremeters { get; set; }
+    public string buildingtype_id { get; set; }
+    public string primaryheatingtype_id { get; set; }
+    public string secondaryheatingtype_id { get; set; }
+}
