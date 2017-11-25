faff800ba07438d99d52215f04123514fb18536f
justasAtGU <gusstirju@student.gu.se>
2017-11-25 11:29:00 +0100

    added enable/disable map

---
+++ EnergyReality/Assets/scripts/HideMap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideMap : MonoBehaviour
+{
+
+    public Button mapButton;
+    public GameObject floorPlan;
+    public GameObject lightPanel;
+
+    bool mapActive = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = mapButton.GetComponent<Button>();
+    }
+
+    public void TaskOnClick()
+    {
+        mapActive = !mapActive;
+        floorPlan.SetActive(mapActive);
+
+        Debug.Log(mapActive);
+        for(int i = 0; i < lightPanel.transform.childCount; i++)
+        {
+            lightPanel.transform.GetChild(i).gameObject.SetActive(mapActive);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
