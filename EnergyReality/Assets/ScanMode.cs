bfe798daf1156c90ed5c500487f49b2ec38d9d71
Shaun McMurray <gusmcmsh@student.gu.se>
2017-11-25 17:38:01 +0100

    hid crosshair

--- EnergyReality/Assets/ScanMode.cs
+++ EnergyReality/Assets/ScanMode.cs
@@ -9,18 +9,21 @@ public class ScanMode : MonoBehaviour
     public Button scanButton;
     public GameObject field1;
     public GameObject field2;
+    public Image image;
 
     bool mapActive = true;
 
     // Use this for initialization
     void Start()
     {
+        image.gameObject.SetActive(false); 
         Button btn = scanButton.GetComponent<Button>();
         //mapActive = false;
     }
 
     public void TaskOnClick()
     {
+        image.gameObject.SetActive(true);
         mapActive = !mapActive;
         field1.SetActive(mapActive);
         field2.SetActive(mapActive);
