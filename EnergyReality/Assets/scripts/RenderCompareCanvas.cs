e4412a23becdb4dd47372837f80cb51a8ce5300d
jeberlen <guseberjo@student.gu.se>
2017-11-25 19:34:41 +0100

    inital

---
+++ EnergyReality/Assets/scripts/RenderCompareCanvas.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RenderCompareCanvas : MonoBehaviour {
+
+    private Button compare;
+    private GameObject compareCanvasPrefab;
+
+    private Transform parent;
+	// Use this for initialization
+	void Start () {
+        compare = GetComponent<Button>();
+        compare.onClick.AddListener(OnClick);
+        
+        compareCanvasPrefab = GameObject.Find("ComparisonCanvas");
+	}
+
+    void OnClick()
+    {
+        parent = compare.GetComponentInParent<Transform>();
+
+        GameObject compareCanvas = Instantiate (compareCanvasPrefab);
+        compareCanvas.transform.SetParent(parent);
+    }
+}
