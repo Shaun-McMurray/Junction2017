9879dfdb315d2fc9e7b518fd05c6910d854afcf4
ElaineQvarnstrom <gusqvael@student.gu.se>
2017-11-25 13:00:54 +0100

    Working startmenu with 2 opt. needs a facial lift

---
+++ EnergyReality/Assets/scripts/LoadScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour {
+
+    private Button loadButton;
+    public string scene; // Add the name of the scene. Script attached to buttons. Update build settings.
+	// Use this for initialization
+	void Start () {
+        loadButton = GetComponent<Button>();
+        loadButton.onClick.AddListener(OnClick);
+	}
+	
+	// Update is called once per frame
+	void OnClick () {
+        SceneManager.LoadScene(scene);
+	}
+}
