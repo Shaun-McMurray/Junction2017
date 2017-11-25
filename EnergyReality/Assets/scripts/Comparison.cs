58c52e9fee99d796aaf929ce59dd36a8031e38d3
jeberlen <guseberjo@student.gu.se>
2017-11-25 16:18:50 +0100

    finshed static

---
+++ EnergyReality/Assets/scripts/Comparison.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using SimpleJSON;
+
+public class Comparison : MonoBehaviour
+{
+
+    private string url = "https://api.priceapi.com/jobs";
+
+    public string value = "";
+
+    void Start ()
+    {
+
+        StartCoroutine(Request());
+
+    }
+
+    IEnumerator Request() {
+
+        WWWForm form = new WWWForm();
+        form.AddField("token", "WMQCDXPTUOIRARBSGPMQTLANXXRDRUDBXBXBUZOMGGBTDVBCMBBFYZXFAPCHTJXT");
+        form.AddField("country", "se");
+        form.AddField("source", "google-shopping");
+        form.AddField("currentness", "daily_updated");
+        form.AddField("completeness", "one_page");
+        form.AddField("key", "keyword");
+        form.AddField("values", value);
+
+        if (value != "")
+        {
+            WWW request = new WWW(url, form);
+
+            yield return request;
+
+            Debug.Log("Request: " + request.text);
+        
+            WWW check = new WWW("https://api.priceapi.com/jobs/" +
+                "5a196fb15fc8fc3c4b23435f?" +
+                "token=WMQCDXPTUOIRARBSGPMQTLANXXRDRUDBXBXBUZOMGGBTDVBCMBBFYZXFAPCHTJXT");
+
+            yield return check;
+
+            if (check.text.Contains("finished"))
+            {
+
+                WWW result = new WWW("https://api.priceapi.com/products/bulk/" +
+                    "5a196fb15fc8fc3c4b23435f?token=WMQCDXPTUOIRARBSGPMQTLANXXRDRUDBXBXBUZOMGGBTDVBCMBBFYZXFAPCHTJXT");
+
+                yield return result;
+                Debug.Log("Result: " + result.text);
+
+            }
+            else
+            {
+                yield return check;
+            }
+
+
+        }
+    }
+}
--- EnergyReality/Assets/scripts/Parser.cs
+++ EnergyReality/Assets/scripts/Parser.cs
@@ -1,4 +1,5 @@
 ﻿using SimpleJSON;
+using System;
 
 
 public class Parser
@@ -19,4 +20,56 @@ public class FortumJson
     public string buildingtype_id { get; set; }
     public string primaryheatingtype_id { get; set; }
     public string secondaryheatingtype_id { get; set; }
+}
+
+public class ComparisonJson
+{
+
+    public class Rootobject
+    {
+        public string job_id { get; set; }
+        public string status { get; set; }
+        public int free_credits { get; set; }
+        public int paid_credits { get; set; }
+        public Product[] products { get; set; }
+    }
+
+    public class Product
+    {
+        public string source { get; set; }
+        public string country { get; set; }
+        public string key { get; set; }
+        public string value { get; set; }
+        public bool success { get; set; }
+        public DateTime updated_at { get; set; }
+        public string id { get; set; }
+        public string condition_filter { get; set; }
+        public string[] gtins { get; set; }
+        public string[] eans { get; set; }
+        public string name { get; set; }
+        public object brand_name { get; set; }
+        public object category_name { get; set; }
+        public int review_count { get; set; }
+        public int review_rating { get; set; }
+        public string url { get; set; }
+        public string image_url { get; set; }
+        public string description { get; set; }
+        public Offer[] offers { get; set; }
+    }
+
+    public class Offer
+    {
+        public string shop_id { get; set; }
+        public string shop_name { get; set; }
+        public string shop_url { get; set; }
+        public object name { get; set; }
+        public string price { get; set; }
+        public string price_with_shipping { get; set; }
+        public string shipping_costs { get; set; }
+        public string currency { get; set; }
+        public object condition { get; set; }
+        public object tax { get; set; }
+        public string url { get; set; }
+    }
+
 }
